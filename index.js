da64ec81e3016b362bb454ececff7e5332979ce9
Oscar H.V <55415043+OscarViquez@users.noreply.github.com>
2022-07-28 09:27:56 -0400

    added mobile phone background for header

---
+++ index.js
@@ -0,0 +1,26 @@
+// var x = 'one';
+
+// function Test() {
+//     console.log(x)
+// }
+
+// Test()
+
+
+
+
+
+// function findLetter() {
+//     let letter = 'x'
+
+//     {
+//         let letter = 'a'
+
+//     }
+//     console.log(letter)
+
+
+// }
+
+
+findLetter()
